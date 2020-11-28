39573e8d196045c8fc7550c18a1380f0326c131b
jatinbharadwaj <jitinrcks@gmail.com>
2020-11-28 05:34:53 +0530

    callback functions

--- src/components/SudokuBoard.js
+++ src/components/SudokuBoard.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import SudokuField from "./SudokuField";
+import Result from "./Result";
 
 export default class SudokuBoard extends Component {
   render() {
-    const { sudoku } = this.props;
+    const { sudoku,onChange} = this.props;
 
     return (
       <div>
+        {!sudoku.solveTime && <Timer start={sudoku.startTime} />}
+        {sudoku.solveTime && <Result sudoku={sudoku} />}
         {sudoku.rows.map(row => (
           <div className="row" key={row.index}>
             {row.cols.map(field => (
-              <SudokuField field={field} key={field.col} />
+              <SudokuField field={field} key={field.col} onChange={onChange} />
             ))}
           </div>
         ))}
